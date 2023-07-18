7f357b8c853508dcd3ce7cb23573dd5d41373540
nomercy <hentai_ai@mail.ru>
2023-07-18 13:10:33 +0300

    Updated ISaveLoadService

--- Assets/Code/Services/SaveLoad/ISaveLoadService.cs
+++ Assets/Code/Services/SaveLoad/ISaveLoadService.cs
@@ -1,7 +1,10 @@
+using Codebase.Data;
+
 namespace Codebase.Services.SaveLoad
 {
 	public interface ISaveLoadService : IService
 	{
-
+		void SaveProgress();
+		PlayerProgress LoadProgress();
 	} 
 }
