f1b50ec67f5c8f53519162e59f8bae22442b6b84
nomercy <hentai_ai@mail.ru>
2023-07-19 11:40:04 +0300

    - Implemented AssetProviderService;

---
+++ Assets/Code/Services/AssetProvider/AssetProviderService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Codebase.Services.AssetProvider
+{
+    public class AssetProviderService : IAssetProviderService
+    {
+        public T Instantiate<T>(string path, Vector3 at) where T : UnityEngine.Object
+        {
+            T prefab = Resources.Load<T>(path);
+
+            if (prefab == null)
+                throw new InvalidOperationException(
+                    $"Asset at path '{path}' is not existed");
+
+            T gameObject = UnityEngine.Object.Instantiate(prefab, at, Quaternion.identity);
+
+            return gameObject;
+        }
+    }
+}
---
+++ Assets/Code/Services/AssetProvider/IAssetProviderService.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Codebase.Services.AssetProvider
+{
+    public interface IAssetProviderService : IService
+    {
+        T Instantiate<T>(string path, Vector3 at) where T : UnityEngine.Object;
+    }
+}
