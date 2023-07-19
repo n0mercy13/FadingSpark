5dc0e130ca8193dbc298394cd9aa32f85d6c073d
nomercy <hentai_ai@mail.ru>
2023-07-19 20:00:29 +0300

    - LoadLevelState implemented;

--- Assets/Code/Infrastructure/StateMachine/LoadLevelState.cs
+++ Assets/Code/Infrastructure/StateMachine/LoadLevelState.cs
@@ -1,8 +1,7 @@
 ﻿using Codebase.Services.Factory;
-using Codebase.Services.PersistentProgress;
-using Codebase.Services.StaticData;
 using Codebase.Services.SceneLoader;
 using Codebase.UI.Factory;
+using System;
 
 namespace Codebase.Infrastructure.StateMachine
 {
@@ -12,17 +11,48 @@ namespace Codebase.Infrastructure.StateMachine
         private readonly ISceneLoaderService _sceneLoader;
         private readonly IPlayerFactory _playerFactory;
         private readonly IUIFactory _uiFactory;
-        private readonly IPersistentProgressService _persistentProgressService;
-        private readonly IStaticDataService _staticDataService;
 
-        public void Enter(string payload)
+        public LoadLevelState(GameStateMachine gameStateMachine, 
+            ISceneLoaderService sceneLoader, 
+            IPlayerFactory playerFactory, 
+            IUIFactory uiFactory)
         {
-            throw new System.NotImplementedException();
+            _gameStateMachine = gameStateMachine;
+            _sceneLoader = sceneLoader;
+            _playerFactory = playerFactory;
+            _uiFactory = uiFactory;
+        }
+
+        public void Enter(string sceneName)
+        {
+            _sceneLoader.Load(sceneName, OnLoaded);
         }
 
         public void Exit()
+        {            
+        }
+
+        private void OnLoaded()
         {
-            throw new System.NotImplementedException();
+            CreatePlayer();
+            CreateUI();
+            //CreateEnemySpawner();
+
+            _gameStateMachine.Enter<GameLoopState>();
+        }
+
+        private void CreateEnemySpawner()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreatePlayer() => 
+            _playerFactory.CreatePlayer();
+
+        private void CreateUI()
+        {
+            _uiFactory.CreateUIRoot();
+            _uiFactory.CreateHUD();
         }
     }
 }
